29e2e0208a2a232ab2990c153767c7a9145a8d9f
mpastorperez <mpastorperez@victoria31.danielcastelao.org>
2017-04-20 12:28:37 +0200

    boletin25

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author mpastorperez
+ */
+public class Boletin25 {
+
+
+    public static void main(String[] args) {
+      //  Ventana vent=new Ventana();
+      // Ventana2 vent2=new Ventana2();
+       //Ventana3 vent3=new Ventana3();
+       Ventana4 vent4=new Ventana4();
+    }
+    
+}
